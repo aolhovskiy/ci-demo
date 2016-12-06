79f4b51b9e64548bf053241ef18278270f3e1e10
Alexey Olhovskiy <alexey.olhovskiy@gmail.com>
2016-12-06 18:41:22 +0200

    Added tests.

---
+++ test/demo.spec.js
@@ -0,0 +1,11 @@
+var expect = require("chai").expect;
+
+describe('Demo', () => {
+  it("Always passes", function() {
+    expect(true).to.equal(true);
+  });
+
+  it("Always fails", function() {
+    expect(true).to.equal(false);
+  });
+});
